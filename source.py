d17c62ebe2090ea29bf9bfe3b1fb818053792672
amircodel <amirali.mohammadi890@gmail.com>
2025-02-01 20:24:36 +0330

    first stable version

--- source.py
+++ source.py
@@ -1,6 +1,7 @@
 import time
 import re
 import csv
+import requests
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
@@ -9,7 +10,7 @@ from selenium.common.exceptions import NoSuchElementException
 from chromedriver_py import binary_path
 
 class Eitaa:
-    def __init__(self, csv_path, init_chat_id, prefix, chrome_profile_path, dev_chat_id, dev_name, message):
+    def __init__(self, csv_path, init_chat_id, prefix, chrome_profile_path, dev_chat_id, message):
         # Initialize the WebDriver
         self.driver_path = binary_path
         self.options = Options()
@@ -20,9 +21,10 @@ class Eitaa:
         self.contact_prefix = prefix
         self.inti_chat_id = init_chat_id
         self.support_chat_id = dev_chat_id
-        self.support_contact_name= dev_name
         self.message_pattern = message
         self.file_dir = csv_path
+        self.count_error = 0
+        self.count_timeout = 0
 
     def get_url(self, url):
         self.driver.get(url)
@@ -35,8 +37,7 @@ class Eitaa:
             self.csv_data = list(csv.reader(f))
 
     def get_item(self, item_index):
-        self.item = [item_index]
-        self.item.extend(self.csv_data[item_index])
+        self.item= self.csv_data[item_index]
     
     def save_contact(self):
 
@@ -99,18 +100,15 @@ class Eitaa:
                 .perform()
             time.sleep(0.25)
 
-            if self.driver.find_element(By.XPATH, "/html/body/div[2]/div[1]/div[2]/div/div/div[2]/div[1]/div/div/div[1]/div/span").text == f"کد {self.item[0]} {self.contact_prefix}":
-                self.item[6]= 1
+            if self.driver.find_element(By.XPATH, "/html/body/div[2]/div[1]/div[2]/div/div/div[2]/div[1]/div/div/div[1]/div/span").text == content:
                 return 1
             else:
-                self.item[6]= 0
                 return 0
         except NoSuchElementException:
-            self.item[6]= 0
             return 0
         
     def get_chat_id(self):
-        self.item.append(re.search(r"#\d+",self.driver.current_url).group())
+        self.item[8] = re.search(r"#\d+",self.driver.current_url).group()
 
     def send_message(self, message):
         try:
@@ -120,7 +118,7 @@ class Eitaa:
                 .perform()
             time.sleep(0.25)
 
-            self.driver.execute_script(f"arguments[0].innerText = arguments[1];", self.driver.find_element(By.XPATH,"/html/body/div[2]/div[1]/div[2]/div/div/div[4]/div/div[1]/div[7]/div[1]/div[1]"), message % (self.item[1], self.item[2]))
+            self.driver.execute_script(f"arguments[0].innerText = arguments[1];", self.driver.find_element(By.XPATH,"/html/body/div[2]/div[1]/div[2]/div/div/div[4]/div/div[1]/div[7]/div[1]/div[1]"), message)
             time.sleep(0.0625)
 
             clickable = self.driver.find_element(By.XPATH, "/html/body/div[2]/div[1]/div[2]/div/div/div[4]/div/div[4]/button/div")
@@ -130,13 +128,10 @@ class Eitaa:
             
             time.sleep(0.25)
             if self.driver.find_element(By.XPATH, "/html/body/div[2]/div[1]/div[2]/div/div/div[4]/div/div[1]/div[7]/div[1]/div[1]").text == "":
-                self.item[7]= 1
                 return 1
             else:
-                self.item[7]= 0
                 return 0
         except NoSuchElementException:
-            self.item[7]= 0
             return 0
 
     def update_csv(self, row):
@@ -176,32 +171,74 @@ class Eitaa:
         time.sleep(0.5)
     
     def report_logs(self, exp):
-        self.get_contact(self.support_contact_name)
-        self.send_message(exp)
+        parse_url = "https://tapi.bale.ai/406394701:1qgLsOLSAqoswcdmLvfTOPpQA6glRlrfEvM5il9O/sendMessage"
+        return requests.post(parse_url, data= {'chat_id':f'{self.support_chat_id}', 'text': f'{exp}'})
 
     def execution(self):
         for i in range(len(self.csv_data)):
-            self.get_item(item_index=i)
-            if self.item[6]:
-                self.save_contact()
-            else:
+            try:
+                self.get_item(item_index=i)
+                if self.item[6]:
+                    self.save_contact()
+                else:
+                    continue
+                if not self.get_contact(f"کد {self.item[0]} {self.contact_prefix}"):
+                    self.item[6]= 0
+                    self.update_csv(row= i)
+                    continue
+                if not self.item[7]:
+                    self.remove_contact()
+                    continue
+                else:
+                    if self.send_message(self.message_pattern  % (self.item[1], self.item[2])):
+                        self.item[7]= 1
+                    else:
+                        raise TypeError("NoMessageSendError")
+                self.get_chat_id()
+                self.remove_contact()
+
+                self.count_error = 0
+                self.update_csv(row= i)
+
+            except NoSuchElementException as exp:
+                self.count_error += 1
+                self.update_csv(row= i)
+                if self.count_error >= 10:
+                    self.report_logs(f"running process stopped during gotten several {exp}")
+                    break
+                
+                self.driver.refresh()
+                time.sleep(0.25)
+                self.driver.refresh()
+                time.sleep(0.5)
+
+                self.read_csv()
+                self.get_url(f"https://web.eitaa.com/#{application.inti_chat_id}")
+                self.execution()
+            except TimeoutError as exp:
+                self.count_timeout += 1
+                self.update_csv(row= i)
+            
+                if self.count_error >= 5:
+                    self.report_logs(f"running process stopped during gotten several {exp}")
+                    break
+
+                time.sleep(600)
+                self.read_csv()
+                self.get_url(f"https://web.eitaa.com/#{application.inti_chat_id}")
+                self.execution()
+            except "NoMessageSendError" as exp:
+                try:
+                    self.report_logs(f"{exp} for record {self.item[0]}")
+                except:
+                    pass
                 continue
-            if not self.get_contact(f"کد {self.item[0]} {self.contact_prefix}"):
-                self.update_csv(self.file_dir, i)
-                continue
-            if not self.item[7]:
-                continue
-            else:
-                self.send_message(self.message_pattern)
-            self.get_chat_id()
-            self.remove_contact()
-            self.update_csv(self.file_dir, i)
 
     def close(self):
         self.driver.quit()
 
 if __name__ == "__main__":
-    application = Eitaa(csv_path="data_csv.csv", init_chat_id="333000", prefix= "اطلاع رسانی بیمه", chrome_profile_path= "user-data-dir=C:\\Users\\amira\\AppData\\Local\\Google\\Chrome\\User Data\\Profile 1", dev_chat_id="", dev_name="", message="همکار محترم دانشگاه علوم پزشکی تبریز %s %s؛\n به استحضار می رساند آخرین مهلت ثبت نام بیمه درمان تکمیلی و عمر و حوادث کارکنان دانشگاه تا پایان وقت اداری مورخ 1403/11/15 می باشد. جهت ثبت نام به رابطین رفاهی مرکز خود مراجعه فرمایید؛ همچنین جهت کسب اطلاعات بیشتر می توانید از نشانی www.bimeh197.ir بازدید فرمایید.")
+    application = Eitaa(csv_path="data_csv.csv", init_chat_id="333000", prefix= "اطلاع رسانی بیمه", chrome_profile_path= "user-data-dir=C:\\Users\\amira\\AppData\\Local\\Google\\Chrome\\User Data\\Profile 1", dev_chat_id= "406394701", message="همکار محترم دانشگاه علوم پزشکی تبریز %s %s؛\n به استحضار می رساند آخرین مهلت ثبت نام بیمه درمان تکمیلی و عمر و حوادث کارکنان دانشگاه تا پایان وقت اداری مورخ 1403/11/15 می باشد. جهت ثبت نام به رابطین رفاهی مرکز خود مراجعه فرمایید؛ همچنین جهت کسب اطلاعات بیشتر می توانید از نشانی www.bimeh197.ir بازدید فرمایید.")
 
     application.read_csv()
     application.get_url(f"https://web.eitaa.com/#{application.inti_chat_id}")
